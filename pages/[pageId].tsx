77351e9aace37d28f38f01d2a3e26cfe7bb99bcf
d2n0s4ur <njh0625@gmail.com>
2024-04-09 18:31:48 +0900

    :construction: In progress

--- pages/[pageId].tsx
+++ pages/[pageId].tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { GetStaticProps } from 'next'
 
 import { NotionPage } from '@/components/NotionPage'
-import { displayCollections, domain, isDev } from '@/lib/config'
+import { domain, isDev } from '@/lib/config'
 import { getSiteMap } from '@/lib/get-site-map'
 import { resolveNotionPage } from '@/lib/resolve-notion-page'
 import { PageProps, Params } from '@/lib/types'
